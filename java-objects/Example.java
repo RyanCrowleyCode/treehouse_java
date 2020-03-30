77860e803623a5b73fcaa10be28e5194e093a3ec
Ryan Crowley <ryancrowleycode@gmail.com>
2020-03-30 12:21:28 -0500

    began java objects lesson in Treehouse

---
+++ java-objects/Example.java
@@ -0,0 +1,13 @@
+
+public class Example {
+ 
+    public static void main(String[] args) {
+      // Your amazing code goes here...
+      
+      System.out.println("We are making a new PEZ Dispenser");
+      PezDispenser dispenser = new PezDispenser();
+      System.out.printf("The dispenser is &s &n",
+                        dispenser.characterName);
+    }
+  
+  }
