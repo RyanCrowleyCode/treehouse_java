073141204ce1868312bd05c5aed8b54526d6b5fb
Ryan Crowley <ryancrowleycode@gmail.com>
2020-03-31 15:52:23 -0500

    case is now irrelevant for guesses and answer, and there is validation in place to make sure a user is typing a letter.

--- java-objects/hangman/Prompter.java
+++ java-objects/hangman/Prompter.java
@@ -8,22 +8,29 @@ class Prompter {
   }
   
   public boolean promptForGuess() {
-    // creating a scanner object
     Scanner scanner = new Scanner(System.in);
-    // printing a line to get input
-    System.out.print("Enter a letter:  ");
-    // using scanner object to read line for input and saving to variable
-    String guessInput = scanner.nextLine();
-    // saving the letter 'char' to a variable called guess
-    char guess = guessInput.charAt(0);
-    // we need to see if the guess matches or not. This is the game's job.
-    // return the boolean from game.applyGuess
     boolean isHit = false;
-    try {
-      isHit =  game.applyGuess(guess);
-    } catch(IllegalArgumentException iae) {
-      System.out.println(iae.getMessage());
-    }
+    boolean isAcceptable = false;
+    
+    
+    do {
+      // printing a line to get input
+      System.out.print("Enter a letter:  ");
+      // using scanner object to read line for input and saving to variable
+      String guessInput = scanner.nextLine();
+      // saving the letter 'char' to a variable called guess
+      char guess = guessInput.charAt(0);
+      // we need to see if the guess matches or not. This is the game's job.
+      // return the boolean from game.applyGuess
+      
+      try {
+        isHit =  game.applyGuess(guess);
+        isAcceptable = true;
+      } catch(IllegalArgumentException iae) {
+        System.out.printf("%s. Please try again. %n",
+                          iae.getMessage());
+      }
+    } while(! isAcceptable);
     
     return isHit;
   }
