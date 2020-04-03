37417e4e1edcd2f249d271024e7d04d1c350319e
Ryan Crowley <ryancrowleycode@gmail.com>
2020-04-03 10:10:59 -0500

    working with different kinds of for loops, added an int array literal

--- java-arrays/Explore.java
+++ java-arrays/Explore.java
@@ -4,8 +4,19 @@ public class Explore {
       // Create a new friends array using the array literal shortcut.
       // Include Ben, Alena, Pasan
       String[] friends = {"Ben", "Alena", "Pasan"};
+      // Enanced for loop
       for (String friend : friends) {
         System.out.printf("Hey %s! The movie starts at 7, C U there!%n", friend);
+      }
+      
+      // Regular for loop
+      for (int i = 0; i < friends.length; i++) {
+        System.out.printf("Hey %s! The movie starts at 7, C U there!%n", friends[i]);
+      }
+      
+      int[] bensScoreCard = {1, 2, 1, 5, 2, 4, 4, 4, 3, 6, 1, 2, 5, 4, 3, 2, 6, 3};
+      for (int i = 0; i < bensScoreCard.length; i++) {
+        System.out.printf("Hole #%d: %d %n", i+1, bensScoreCard[i]);
       }
       
     }
